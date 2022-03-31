30915de3cc0a6580ee10a83258448cee9091ff7b
yuetong3yu <yuetong3yu@gmail.com>
2022-03-31 17:53:19 +0800

    say hi

--- frontend-web3-counter/src/App.js
+++ frontend-web3-counter/src/App.js
@@ -1,5 +1,26 @@
+import { useState, useEffect } from 'react'
+
 function App() {
-  return <div className="text-5xl text-center text-blue-800">Hello World!</div>
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="w-full min-h-screen bg-blue-900 flex flex-col justify-center items-center">
+      <h1 className="text-8xl font-bold text-white text-center text-shadow">
+        Hello Web3!
+      </h1>
+
+      <h2 className="text-6xl text-center my-24 font-bold text-yellow-300">
+        👋 {count}
+      </h2>
+
+      <button
+        className="rounded-full py-6 px-12 text-3xl mt-16 text-white bg-purple-700 hover:scale-105 hover:bg-purple-600 transition-all"
+        onClick={() => setCount((s) => ++s)}
+      >
+        Say Hi
+      </button>
+    </div>
+  )
 }
 
 export default App
